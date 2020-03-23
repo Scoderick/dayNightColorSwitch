a5e647199f1ab72053844198441cb3c09bec9ec9
Scoderick <62555359+Scoderick@users.noreply.github.com>
2020-03-23 11:18:38 -0500

    First working version

---
+++ #Night colors.py
@@ -0,0 +1,67 @@
+#Night colors
+values = [	'True',
+			'#004A7C',
+			'#FFFFFF',
+			'#004A7C',
+			'#FFFFFF',
+			'223,222,222',
+			'60,132,180',
+			'#E5E5E5',
+			'#000000',
+			'#004A7C',
+			'#505050',
+			'#FFFFFF',
+			'#FFFFFF',
+			'#004A7C']	
+paths = [
+		'[Grimes01]Styles/DayNight',
+		'[Grimes01]Styles/FooterBackground',
+		'[Grimes01]Styles/FooterFontColor',
+		'[Grimes01]Styles/HeaderBackground',
+		'[Grimes01]Styles/HeaderFontColor',
+		'[Grimes01]Styles/NoEditBlue',
+		'[Grimes01]Styles/NoEditGray',
+		'[Grimes01]Styles/PageBackground',
+		'[Grimes01]Styles/PageFontColor',
+		'[Grimes01]Styles/PopUpBackground',
+		'[Grimes01]Styles/PopUpBorderColor',
+		'[Grimes01]Styles/PopUpFontColor',
+		'[Grimes01]Styles/RepublicsBlue',
+		'[Grimes01]Styles/White']
+system.tag.writeBlocking(paths,values)
+
+values = 
+	[ 'True',
+	  '#FFFFFF',
+	  '#004A7C',
+	  '#FFFFFF',
+	  '#004A7C',
+	  '60,132,180',
+	  '223,222,222',
+	  '#E5E5E5',
+	  '#000000',
+	  '#FFFFFF',
+	  '#505050',
+	  '#004A7C',
+	  '#004A7C',
+	  '#FFFFFF']	
+paths = 
+	[ '[Grimes01]Styles/DayNight',
+	  '[Grimes01]Styles/FooterBackground',
+	  '[Grimes01]Styles/FooterFontColor',
+	  '[Grimes01]Styles/HeaderBackground',
+	  '[Grimes01]Styles/HeaderFontColor',
+	  '[Grimes01]Styles/NoEditBlue',
+	  '[Grimes01]Styles/NoEditGray',
+	  '[Grimes01]Styles/PageBackground',
+	  '[Grimes01]Styles/PageFontColor',
+	  '[Grimes01]Styles/PopUpBackground',
+	  '[Grimes01]Styles/PopUpBorderColor',
+	  '[Grimes01]Styles/PopUpFontColor',
+	  '[Grimes01]Styles/RepublicsBlue',
+	  '[Grimes01]Styles/White']
+
+
+
+
+
